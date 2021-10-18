ffe449ef8aedf256d21eac3a5c54ead9679e180f
Renma Hirano <everydaysai@gmail.com>
2021-10-18 17:03:04 +0900

    WAVを再生できるようにした

---
+++ heartbeat.py
@@ -0,0 +1,33 @@
+### wavファイルを再生するコード
+
+import numpy as np
+import wave 
+import pyaudio
+import matplotlib.pyplot as plt
+
+def PlayWavFie(Filename = "Heart001.wav"):
+    try:
+        wf = wave.open(Filename, "r")
+    except FileNotFoundError: #ファイルが存在しなかった場合
+        print("[Error 404] No such file or directory: " + Filename)
+        return 0
+        
+    # ストリームを開く
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+
+    # 音声を再生
+    chunk = 1024
+    data = wf.readframes(chunk)
+    while data != '':
+        stream.write(data)
+        data = wf.readframes(chunk)
+    stream.close()
+    p.terminate()
+
+
+if __name__ == "__main__":
+    PlayWavFie("Heart001.wav") 
