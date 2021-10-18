f6b5ff99e5c54e38a7144df7b8bc8e961a125130
Renma Hirano <everydaysai@gmail.com>
2021-10-18 17:09:16 +0900

    refactor:change comment

--- wavtonumpy.py
+++ wavtonumpy.py
@@ -1,15 +1,22 @@
 ### wavファイル -> numpy array -> plot & play sound(from array)
 ### 注意：大抵のWAVファイルは2chですが、1chしか出力していません。
 
-import soundfile as sf                      # wavファイルの取り扱いに使用
-import numpy as np                          # 配列処理に使用
-from matplotlib import pyplot as plt        # グラフ表示に使用
+import soundfile as sf
+import numpy as np
+from matplotlib import pyplot as plt
 import pyaudio
 
-path = 'Heart001.wav'                       # wavファイルまでのパスを指定（名前はお手持ちのファイル名に変更して下さい）
-data, samplerate = sf.read(path)            # wavファイルを読み込む
+# read wav file
+path = 'Heart001.wav'
+data, samplerate = sf.read(path)
 
-t = np.arange(0, len(data)) / samplerate    # グラフ表示のための横軸を設定
+# グラフ表示のための横軸を設定
+t = np.arange(0, len(data)) / samplerate
+
+# グラフ表示
+plt.plot(t, data[:,0])
+plt.show()
+plt.close()
 
 # 音声を出力するためのストリームを開く
 p = pyaudio.PyAudio()
@@ -19,11 +26,7 @@ stream = p.open(format=pyaudio.paFloat32,
                 frames_per_buffer=1024,
                 output=True)
                 
+# ch2(data) -> ch1(data[:,0])
 print(data[:,0])
 stream.write(data[:,0].astype(np.float32).tobytes())
 stream.close()
-
-# グラフ表示
-plt.plot(t, data[:,0])
-plt.show()
-plt.close()
