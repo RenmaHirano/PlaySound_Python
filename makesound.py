aa3d27914a5b7c900118fe2f28a3654b5e7563de
Renma Hirano <everydaysai@gmail.com>
2021-11-25 10:18:44 +0900

    Add New Parameter

--- makesound.py
+++ makesound.py
@@ -11,13 +11,13 @@ WAVE_END_TIME = 1  # 秒の振動になる
                  # ここからは2音に関わるパラメータ
 DEFAULT_SECOND_WAVE_SHIFT = 0.3 # 心音が1秒に1回なると仮定した際、1音と2音がなる間の時間(sec)
                      # 普通は大体0.3秒くらいらしい
-SECOND_WAVE_LOUDNESS = 0.3 # 1音に対する音量の比。たとえば0.1なら1/10の音量になります。
+DEFAULT_SECOND_WAVE_LOUDNESS = 0.3 # 1音に対する音量の比。たとえば0.1なら1/10の音量になります。
 
-#GAIN_LIST = [5, 10, 20]
-GAIN_LIST = [2]
+GAIN_LIST = [1.0]
+GAIN_RATIO_LIST = [0.2, 0.45, 0.7]
+ATTENUATION_LIST = [10, 20, 30]
 FREQ_LIST = [30, 100, 300]
-ATTENUATION_LIST = [10, 30]
-PHI_LIST = [0.15, 0.35]
+PHI_LIST = [0.35, 0.4, 0.45]
 
 # 音声を出力するためのストリームを開く
 def OpenStream():
@@ -30,42 +30,52 @@ def OpenStream():
     return stream
 
 # 心音出力関数(gain * sin(2*PIE*frequency*t)*exp(-attenuationRate * t)の減衰正弦波形)
-def GenerateHeartbeat(gain :float, frequency: float, attenuationRate: float, secondWaveShift: float, saveSoundOption = False):
+def GenerateHeartbeat(gain :float, gainRatio :float, frequency: float, attenuationRate: float, secondWaveShift: float, saveSoundOption = False):
     time = np.arange(RATE*WAVE_END_TIME) / RATE  # = t
     attenuationArray = np.exp(-1 * attenuationRate * time) # = exp(-Bt)
     basicWave = gain * np.sin(2 * np.pi * frequency * time) * attenuationArray  # = A*sin(2*PIE*f*t)*exp(-Bt)
     firstWave = basicWave
-    secondWave = SECOND_WAVE_LOUDNESS * np.roll(basicWave, int(RATE * WAVE_END_TIME * secondWaveShift))
+    secondWave = gainRatio * np.roll(basicWave, int(RATE * WAVE_END_TIME * secondWaveShift))
     heartBeatWave = firstWave + secondWave
     
     if saveSoundOption:
         nowTime = dt.datetime.now().strftime('%Y%m%d-%H%M%S')
         out = np.tile(heartBeatWave, REPEAT_TIMES)
         
-        freqString = "";
+        freqString = ""
         if frequency == FREQ_LIST[0]:
-            freqString = "Low";
+            freqString = "Low"
         if frequency == FREQ_LIST[1]:
-            freqString = "Medium";
+            freqString = "Medium"
         if frequency == FREQ_LIST[2]:
-            freqString = "High";
+            freqString = "High"
 
-        attenuationString = "";
+        attenuationString = ""
         if attenuationRate == ATTENUATION_LIST[0]:
-            attenuationString = "Low";
+            attenuationString = "Low"
         if attenuationRate == ATTENUATION_LIST[1]:
-            attenuationString = "High";
+            attenuationString = "Medium"
+        if attenuationRate == ATTENUATION_LIST[2]:
+            attenuationString = "High"
 
-        phiString = "";
+        phiString = ""
         if secondWaveShift == PHI_LIST[0]:
-            phiString = "Narrow";
+            phiString = "Narrow"
         if secondWaveShift == PHI_LIST[1]:
-            phiString = "Wide";
+            phiString = "Medium"
+        if secondWaveShift == PHI_LIST[2]:
+            phiString = "High"
+        
+        gainRatioString = ""
+        if gainRatio == GAIN_RATIO_LIST[0]:
+            gainRatioString = "Narrow"
+        if gainRatio == GAIN_RATIO_LIST[1]:
+            gainRatioString = "Medium"
+        if gainRatio == GAIN_RATIO_LIST[2]:
+            gainRatioString = "High"
             
-        # write(str(gain) + "_" + str(frequency) + "_" + str(attenuationRate) + ".wav" , RATE, out.astype(np.float32))
-        write(freqString + attenuationString + phiString + ".wav" , RATE, out.astype(np.float32))
-        #write("output" + nowTime + "_Gain" + str(gain) + "_Freq" + str(frequency) + "_Atten" + str(attenuationRate) + "_Phi" + str(secondWaveShift) + ".wav" , RATE, out.astype(np.float32))
-    
+        write(gainRatioString + freqString + attenuationString + phiString + ".wav" , RATE, out.astype(np.float32))
+        
     return heartBeatWave
 
 # 図出力部分
@@ -89,7 +99,14 @@ def main():
         for frequency in FREQ_LIST:
             for attenuationRate in ATTENUATION_LIST:
                 for phi in PHI_LIST:
-                    wave = GenerateHeartbeat(gain, frequency, attenuationRate, phi, True)
+                    for gainRatio in GAIN_RATIO_LIST:
+                        wave = GenerateHeartbeat(gain, gainRatio, frequency, attenuationRate, phi, True)
+                        plt.title("A="+ str(gain) +", B="+ str(attenuationRate) + ", C=" + str(round(gainRatio,1)) +  ", phi=" + str(phi) + ", f=" + str(frequency), fontsize = 16)
+                        plt.ylim(-1, 1)
+                        plt.yticks([-1.0, -0.5, 0.0, 0.5, 1.0])
+                        plt.xlim(0, RATE)
+                        plt.xticks([0, RATE/4, RATE*2/4, RATE*3/4, RATE])
+                        PlotGraph(wave)
     
     pass
 
